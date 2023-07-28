77fac45ae16736e5fcc3020dbe78fd058984c1e8
KaleChips22 <95776366+KaleChips22@users.noreply.github.com>
2023-07-28 19:21:01 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,35 +8,52 @@ function startTime() {
     h = checkTime(h)
     m = checkTime(m)
     s = checkTime(s)
+	if (dispType == 12) {
+		let ampm = h >= 12 ? 'PM' : 'AM'
 
-    let ampm = h >= 12 ? 'PM' : 'AM'
-
-    h = h > 12 ? h - 12 : (h == '00' ? 12 : h)
-    clock.innerHTML = `${h}:${m}${seconds ? `:${s}` : ''} ${ampm}`
-
-    let t = setTimeout(startTime, 500)
+		h = h > 12 ? h - 12 : (h == '00' ? 12 : h)
+	}
+    clock.innerHTML = `${h}:${m}${seconds ? `${s}` : ''}${dispType == 12 ? ` ${ampm}` : ''}`
+	let t = setTimeout(startTime, 500)
 }
 
 function checkTime(i) {
-    return i < 10 ? '0' + i : i
+	return i < 10 ? '0' + i : i
 }
 
-let seconds = true
+let seconds = true,
+	dispType = 12
 startTime()
 
 function hideSeconds() {
     seconds = !seconds
     document.getElementById('hide').innerHTML = `${seconds ? 'Hide' : 'Show'} Seconds`
-    window.localStorage.setItem('preferences-show-seconds',  seconds ? 'true' : 'false')
+    pref('show-seconds',  seconds ? 'true' : 'false')
+}
+
+function time24hour() {
+    dispType = (dispType == 24 ? 12 : 24)
+    document.getElementById('disptype').dataset.selected = (dispType == 24)
+    pref('disp-type', dispType)
+}
+
+function pref(id, value = null) {
+    if (value == null) return getP(`preferences-${id}`)
+    
+    // if there is a value to set
+	return window.localStorage.setItem(`preferences-${id}`, value)
 }
 
 function getP(cname) {
-    return window.localStorage.getItem(cname) || ''
+    let c = window.localStorage.getItem(cname) || return ''
+    if (isNaN(parseInt(c))) return c
+    return parseInt(c)
 }
 
-seconds = getP('preferences-show-seconds') == 'false' ? false : true
+seconds = pref('show-seconds') == 'false' ? false : true
+dispType = pref('dips-type')
 document.getElementById('hide-seconds').innerHTML = `${seconds ? 'Hide' : 'Show'} Seconds`
-let lightTheme = getP('preferences-light-theme') == 'true' ? true : false
+let lightTheme = pref('light-theme') == 'true' ? true : false
 if (lightTheme)
     document.body.classList.add('light')
 
@@ -51,5 +68,5 @@ function swapThemes() {
     } else {
         document.body.classList.add('light')
     }
-    window.localStorage.setItem('preferences-light-theme',  document.body.classList.contains('light') ? 'true' : 'false')
+    pref('light-theme',  document.body.classList.contains('light') ? 'true' : 'false')
 }
